f0e1b87ef8a820af26113ed54a631c296952153d
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2025-11-21 09:07:57 +0100

    creación proyecto evaluacion

---
+++ ejercicio_evaluacion/src/test/java/com/salesianostriana/dam/ejercicio_evaluacion/EjercicioEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejercicio_evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
